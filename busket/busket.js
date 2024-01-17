99361b3c891c6f58484218f1534040894953fa3a
SLulya <74452184+SLulya@users.noreply.github.com>
2024-01-17 17:01:25 +0200

    Update busket.js

--- busket/busket.js
+++ busket/busket.js
@@ -96,11 +96,7 @@ let cartidx;
     localStorage.setItem('checkedarr', JSON.stringify(prodscheck))
     drawprods()
 }
-    let gottotalprice = localStorage.getItem("totalprice");
-    if(gottotalprice == null){
-         document.getElementById('numberofbooks').innerHTML  = 
-        `<h2 class="text" >Всього: 0 </h2>`;
-    }else{
+    
 sum = 0;
 prodscheck.forEach(prod =>{
     sum += Number(prod.price*prod.count);
@@ -109,7 +105,7 @@ prodscheck.forEach(prod =>{
         let suma = localStorage.getItem('totalprice');
         document.getElementById('numberofbooks').innerHTML  = 
         `<h2 class="text" >Всього: ${suma} </h2>`;
-    }
+    
 localStorage.setItem('checkedarr', JSON.stringify(prodscheck));
 
 
