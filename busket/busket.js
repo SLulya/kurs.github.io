a0916127560ba44aba121ed3d9876dfc849c9ecf
SLulya <74452184+SLulya@users.noreply.github.com>
2024-01-17 18:16:54 +0200

    Update busket.js

--- busket/busket.js
+++ busket/busket.js
@@ -221,7 +221,7 @@ function sam(){
 function cancel(){
         let buynow = document.getElementById('modal');
         buynow.style.display = "none";
-    localStorage.removeItem("sumawithdel);
+    localStorage.removeItem('sumawithdel');
       location.reload();
 }
 
